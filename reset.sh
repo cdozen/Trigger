cc62c28b73ee5f5f861ad5da4cdf9607869918b9
Candan Dozen <candan.dozen@cern.ch>
2015-07-21 04:01:49 +0200

    initial commit

---
+++ reset.sh
@@ -0,0 +1,12 @@
+v4l2-ctl -c brightness=128
+v4l2-ctl -c contrast=32
+v4l2-ctl -c saturation=32
+v4l2-ctl -c white_balance_temperature_auto=1
+v4l2-ctl -c gain=64
+v4l2-ctl -c power_line_frequency=2
+v4l2-ctl -c white_balance_temperature=4000
+v4l2-ctl -c sharpness=24
+v4l2-ctl -c backlight_compensation=0
+v4l2-ctl -c exposure_auto=3
+v4l2-ctl -c exposure_absolute=166
+v4l2-ctl -c exposure_auto_priority=0
