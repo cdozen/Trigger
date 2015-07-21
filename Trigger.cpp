eaba757fc972ba724d9f5481d305189a3fd3ed39
Candan Dozen <candan.dozen@cern.ch>
2015-07-21 04:01:27 +0200

    update Trigger.cpp

--- Trigger.cpp
+++ Trigger.cpp
@@ -3,67 +3,94 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
 using namespace cv;
 using namespace std;
 
-const int width  = 640;
-const int height = 480;
+const int width  = 640*2;
+const int height = 480*2;
 
-int pixelValueThreshold = 180;
-int numOfPixelThreshold = 5000;
+int pixelValueThreshold = 150;
+int numOfPixelThreshold = 50;
 int numOfPixel;
-int exposure = 500;
+int exposure = 10000;
 
-Mat frame, gray, tIm;
+Mat frame, gray, tIm, frame2, tIm2;
 
 int frameNumber = 0;
 void save() {
     char fileName[128];
-    sprintf(fileName, "output/frame_%d.png", frameNumber);
+    sprintf(fileName, "output/frame_%d.tiff", frameNumber);
     if(imwrite(fileName, frame)) printf("Image saved: %s\n", fileName);
     else printf("Could not be saved: %s\n", fileName);
     frameNumber ++;
 }
 
+double getMS(struct timeval tv){
+    return (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+}
+
 int main() {
-    VideoCapture stream1(0);   //0 is the id of video device.0 if you have only one camera.
+    // old time stamp for time counter
+    struct timeval tv;
+    double old;
+  //camera settings  
     system("v4l2-ctl -c exposure_auto_priority=0");
     system("v4l2-ctl -c exposure_auto=1");
+/*
+    system("v4l2-ctl -c backlight_compensation=0");
+    system("v4l2-ctl -c power_line_frequency=0");
+    system("v4l2-ctl -c gain=255");
+    system("v4l2-ctl -c contrast=255");
+    system("v4l2-ctl -c brightness=242");
+    system("v4l2-ctl -c saturation=32");
+    system("v4l2-ctl --set-fmt-video=width=1280,height=960,pixelformat=0"); 
+    system("v4l2-ctl -c white_balance_temprature_auto=0");
+    system("v4l2-ctl -c --set-parm=1");
+*/
 
- 
-    if (!stream1.isOpened()) { //check if video device has been initialised
-        cout << "cannot open camera";
-        return 1;
-    }
-
+    // cout<<system("v4l2-ctl -d /dev/video0 --list-formats")<<endl;    
+    
+    VideoCapture stream1 (0);   //0 is the id of video device.0 if you have only one camera.
+    
+    stream1.set(CV_CAP_PROP_FRAME_WIDTH, width);
+    stream1.set(CV_CAP_PROP_FRAME_HEIGHT, height);
+    
     namedWindow("Trigger", 1);
 
     // add trackbar
     createTrackbar("Threshold (Pixel Value):", "Trigger", &pixelValueThreshold, 255, NULL);
     createTrackbar("Num of active pixel:",     "Trigger", NULL, width * height, NULL);
-    createTrackbar("Threshold (num of pix):",  "Trigger", &numOfPixelThreshold, width * height, NULL);
-    createTrackbar("Exposure:",  "Trigger", &exposure, 50*1000, NULL);
+    createTrackbar("Threshold (num of pix):",  "Trigger", &numOfPixelThreshold, 100000, NULL);
+    createTrackbar("Exposure:",  "Trigger", &exposure, 10000, NULL);
  
     //unconditional loop
     char exposureCommand[128];
     while (true) {
         sprintf(exposureCommand, "v4l2-ctl -c exposure_absolute=%d", exposure);
         system(exposureCommand);
-        //cout << stream1.get(CV_CAP_PROP_EXPOSURE) << endl;
+        //system("v4l2-ctl --get-ctrl=exposure_absolute");
+        //system("v4l2-ctl --device=/dev/video0 --get-ctrl=exposure_absolute");
+        gettimeofday(&tv, NULL);
+        old = getMS(tv);
         stream1.read(frame);
-        //// convert image into gray scale
+        gettimeofday(&tv, NULL);
+        cout << "Time diff(ms): " << getMS(tv) - old << endl;
+        // convert image into gray scale
         cvtColor(frame, gray, CV_BGR2GRAY);
-        //// pass throught threshold
+        // pass throught threshold
         threshold(gray, tIm, pixelValueThreshold, 255, 0);
-        //inRange(frame, cv::Scalar(0, 0, 0), cv::Scalar(pixelValueThreshold, pixelValueThreshold, pixelValueThreshold), tIm);
         // count active pixels
         numOfPixel = countNonZero(tIm);
         setTrackbarPos("Num of active pixel:", "Trigger", numOfPixel);
-        cout << pixelValueThreshold << endl;
-        // cout << "Number of active pixel: " << numOfPixel << endl; 
-        imshow("Trigger", tIm);
-        imshow("Webcam", frame);
-        if(numOfPixel > numOfPixelThreshold) save();
+        Size size(frame.cols/2.5, frame.rows/2.5);
+        resize(tIm, tIm2, size);
+        resize(frame, frame2, size);
+        imshow("Trigger", tIm2);
+        imshow("Webcam", frame2);
+        if(numOfPixel > numOfPixelThreshold) 
+        save();
         if (waitKey(30) >= 0)
             break;
     }
